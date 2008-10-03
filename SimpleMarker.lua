4ac9bd9680540d8fae9e8ba357ade67b06349eb7
quaiche <quaiche@d0f0cad5-38a1-4d3f-9a6a-ec2700baf368>
2008-10-03 16:09:15 +0000

    Update author fields

--- SimpleMarker.lua
+++ SimpleMarker.lua
@@ -1,7 +1,7 @@
 --[[
 SimpleMarker/SimpleMarker.lua
 
-Copyright 2008 Peter Provost
+Copyright 2008 Quaiche
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
